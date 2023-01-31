ee1ab2b1f9c1f8e8c4206969bb8f6b5a3e07bc1a
Matt DeKrey <mattdekrey@gmail.com>
2023-01-31 07:53:31 -0600

    Update to .NET 7

--- images/DeKreyConsulting.StaticFileServer/Program.cs
+++ images/DeKreyConsulting.StaticFileServer/Program.cs
@@ -2,11 +2,35 @@ using CompressedStaticFiles;
 using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#pragma warning disable ASP0013 // suprress suggestion to use
+builder.Host.ConfigureAppConfiguration((context, config) => {
+    foreach (var s in config.Sources.OfType<FileConfigurationSource>())
+    {
+        s.ReloadOnChange = false;
+    }
+});
+#pragma warning restore ASP0013
+
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.All;
 });
 builder.Services.AddCompressedStaticFiles();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddJsonConsole(options =>
+{
+    options.IncludeScopes = false;
+    options.TimestampFormat = "yyyy:MM:dd hh:mm:ss ";
+    options.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
+    {
+        // sometimes useful to change this to true when testing locally.
+        // but it needs to be false for Fluent Bit to
+        // process log lines correctly
+        Indented = false
+    };
+});
 
 var app = builder.Build();
 
